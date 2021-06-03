7ae4c8da18d9e7b1f8f959e745e92b749483a46a
승철 차 <cscculture95@gmail.com>
2021-06-03 21:03:53 +0900

    프로젝트 전체적 코드 merge

--- Service/routes/page.js
+++ Service/routes/page.js
@@ -6,7 +6,7 @@ const User = require('../models/user');
 var connection = mysql.createConnection({
     host     : 'localhost',
     user     : 'root',
-    password : 'keAarwrm76*',
+    password : '',
     database : 'capstone',
     port:'3306'
   });
